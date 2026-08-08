637add673858d30e1e93095fa36655f6317d0546
raanvixtechnologies <raanvixtechnologies@gmail.com>
2026-08-08 10:44:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,7 @@ export default function App() {
         <div className="container">
           <div className="feature-split">
             <div className="feature-image">
-              <img src="public\ledscreen.jpg" alt="LED Wall Setup" loading="lazy" />
+              <img src="https://github.com/raanvixtechnologies/matrix_av/blob/main/public/ledscreen.jpg" alt="LED Wall Setup" loading="lazy" />
             </div>
             <div className="feature-content">
               <h2>Considering an LED wall for your next event?</h2>
