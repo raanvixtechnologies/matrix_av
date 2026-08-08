537a37a7d77e875a5b95234c882b65434feb38cd
raanvixtechnologies <raanvixtechnologies@gmail.com>
2026-08-08 10:27:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ export default function App() {
         <div className="container">
           <div className="feature-split reverse">
             <div className="feature-image">
-              <img src="\public\ledscreen.png" alt="Equipment Rentals" loading="lazy" />
+              <img src="public\ledscreen.png" alt="Equipment Rentals" loading="lazy" />
             </div>
             <div className="feature-content">
               <h2>Equipment Rentals Made Easy</h2>
